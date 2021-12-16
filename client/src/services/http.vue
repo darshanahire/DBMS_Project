22c71c15da76a343eb181d909ad624fe34f098d4
akash_ghadge <asghadge6@gmail.com>
2021-12-16 11:40:18 +0530

    login page layout added

---
+++ client/src/services/http.vue
@@ -0,0 +1,15 @@
+<script>
+import axios from "axios";
+
+export default {
+  name: "http",
+  baseUrl: "",
+  async loginAsBank(payload) {
+    return await axios.post(this.baseUrl + "/login/bank", payload);
+  },
+
+  async loginAsUser(payload) {
+    return await axios.post(this.baseUrl + "/login/user", payload);
+  },
+};
+</script>
---
+++ client/src/views/Login.vue
@@ -0,0 +1,59 @@
+<template>
+  <div class="container">
+    <label for="">username :</label>
+    <input type="text" name="username" v-model="username" />
+    <br />
+    <label for="">password :</label>
+    <input type="text" name="password" v-model="password" />
+    <br />
+    <button @click="loginUser">Login User</button>
+    <button @click="loginBank">Login Bank</button>
+    <br />
+    <a href="">create account</a>
+  </div>
+</template>
+
+<script>
+import http from "@/services/http.vue";
+export default {
+  name: "Login",
+  data() {
+    return {
+      username: "",
+      password: "",
+    };
+  },
+  methods: {
+    loginUser() {
+      http
+        .loginAsUser(this.payload)
+        .then((data) => {
+          console.log(data.data);
+        })
+        .catch((err) => {
+          console.log(err);
+        });
+    },
+    loginBank() {
+      http
+        .loginAsBank(this.payload)
+        .then((data) => {
+          console.log(data.data);
+        })
+        .catch((err) => {
+          console.log(err);
+        });
+    },
+  },
+  computed: {
+    payload() {
+      return {
+        username: this.username,
+        password: this.password,
+      };
+    },
+  },
+};
+</script>
+<style>
+</style>
