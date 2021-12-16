d7ffc1122fc8296c87eb5fa4865fdb8ca521b430
Darshan Ahire <72602965+darshanahire@users.noreply.github.com>
2021-12-16 15:44:40 +0530

    login api created

--- routes/route.js
+++ routes/route.js
@@ -1,6 +1,7 @@
 const router = require('express').Router()
 const db = require('../db/conn');
 
+const verify = require("../middleware/verify")
 router.get('/createDatabase', (req, res) => {
     let sql_query = 'CREATE DATABASE Bank';
     db.query(sql_query, (err, result) => {
@@ -12,7 +13,7 @@ router.get('/createDatabase', (req, res) => {
 });
 
 router.get('/createUsersDetailsTable', (req, res) => {
-    let sql_query = 'CREATE TABLE usersDetails(username VARCHAR(255), password VARCHAR(255), acc_no INT, email VARCHAR(255), mobile_no VARCHAR(255), acc_bal INT, PRIMARY KEY(username))';
+    let sql_query = 'CREATE TABLE usersDetails(username VARCHAR(255), password VARCHAR(255), acc_no VARCHAR(255), email VARCHAR(255), mobile_no VARCHAR(255), acc_bal INT, PRIMARY KEY(username))';
     db.query(sql_query, (err, result) => {
         if (err) {
             console.log(err)
@@ -31,7 +32,7 @@ router.post('/createAcc', async (req, res) => {
     const sql_query = 'INSERT INTO usersDetails SET ?'
     db.query(sql_query, body, (err, result) => {
         if (err)
-            throw err;
+            console.log(err);
         else {
             console.log(result);
             res.status(200).json("Account Created");
@@ -39,4 +40,16 @@ router.post('/createAcc', async (req, res) => {
     })
 });
 
+router.post('/login/user', async (req, res) => {
+    const { username, password } = req.body;
+    const sql_query = "SELECT * FROM usersDetails WHERE username='" + username + "' AND password='" + password + "'";
+
+    db.query(sql_query, (err, result) => {
+        if (err)
+            console.log(err);
+        else {
+            res.status(200).json(result);
+        }
+    })
+})
 module.exports = router;
--- server.js
+++ server.js
@@ -4,6 +4,8 @@ const app = express();
 const port = process.env.PORT || 5000;
 const router = require('./routes/route')
 
+const cors = require("cors");
+app.use(cors());
 app.use(express.json());
 app.use(router);
 
