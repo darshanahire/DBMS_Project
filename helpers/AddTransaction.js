6da8332a1ebc8d8bb4e54971d3eaf9678e9bbd8c
akash_ghadge <asghadge6@gmail.com>
2021-12-22 15:22:39 +0530

    Transfer api added

--- helpers/AddTransaction.js
+++ helpers/AddTransaction.js
@@ -26,16 +26,82 @@ function isInSufficientBalance(username, amount) {
         const sqlQueryFind = `select * from usersDetails where username="${username}";`
         db.query(sqlQueryFind, (err, currUser) => {
             if (err) {
-                console.log(err);
-                return res.status(500).json(err);
+                return reject(err);
             }
             let acc_bal = currUser[0].acc_bal;
             if (acc_bal - amount < 0)
-                return reject();
+                return reject("Insufficient Balance !!!");
             else
                 return resolve();
         })
     })
 }
 
-module.exports = { AddTransaction,isInSufficientBalance }
+function DepositAmount(username, amount) {
+    return new Promise((resolve, reject) => {
+        const sqlDeposit = `update usersDetails set acc_bal=acc_bal+${amount} where username="${username}";`
+        db.query(sqlDeposit, (err, response) => {
+            if (err) return reject(err);
+            const payload = {
+                amount: amount,
+                type: "deposit",
+                username: username
+            }
+            AddTransaction(payload)
+                .then((transactionAdded) => {
+                    return resolve(transactionAdded);
+                })
+                .catch((err) => {
+                    return reject(err);
+                })
+        })
+    })
+}
+
+function WithdrawAmount(username, amount) {
+    return new Promise((resolve, reject) => {
+        const sqlQueryUpdate = `update usersDetails set acc_bal=acc_bal - ${amount} where username="${username}";`
+
+        // balance check
+        isInSufficientBalance(username, amount)
+            .then((data) => {
+                db.query(sqlQueryUpdate, (err, response) => {
+                    if (err) return reject(err);
+                    const payload = {
+                        amount: amount,
+                        type: "withdraw",
+                        username: username
+                    }
+                    AddTransaction(payload)
+                        .then((transactionAdded) => {
+                            resolve("Withdraw Successful !!!");
+                        })
+                        .catch((err) => {
+                            reject(err);
+                        })
+                })
+            })
+            .catch((err) => {
+                reject(err);
+            })
+    })
+}
+
+function TransferAmount(username, userToSend, amount) {
+    return new Promise((resolve, reject) => {
+        WithdrawAmount(username, amount)
+            .then((withdrawData) => {
+                DepositAmount(userToSend, amount)
+                    .then((depositData) => {
+                        resolve(withdrawData, depositData);
+                    })
+                    .catch((err) => {
+                        reject(err);
+                    })
+            })
+            .catch((err) => {
+                reject(err);
+            })
+    })
+}
+module.exports = { AddTransaction, isInSufficientBalance, DepositAmount, WithdrawAmount, TransferAmount };
--- routes/transactions.js
+++ routes/transactions.js
@@ -1,66 +1,44 @@
-const { Router } = require("express");
+const { Router, response } = require("express");
 const router = Router();
-const { AddTransaction, isInSufficientBalance } = require("../helpers/AddTransaction");
+const { AddTransaction, isInSufficientBalance, DepositAmount, WithdrawAmount, TransferAmount } = require("../helpers/AddTransaction");
 const db = require("../db/conn");
 
 router.post("/deposit", (req, res) => {
     const { depositAmount } = req.body;
     const username = res.locals.username;
-    const sqlQuery = `update usersDetails set acc_bal=acc_bal+${depositAmount} where username="${username}";`
-    db.query(sqlQuery, (err, response) => {
-        if (err) {
-            console.log(err);
-            return res.status(500).json(err);
-        }
-        let payload = {
-            amount: depositAmount,
-            type: "deposit",
-            username: username
-        }
-        AddTransaction(payload)
-            .then((transactionAdded) => {
-                res.status(203).json(response);
-            })
-            .catch((err) => {
-                console.log(err);
-                res.status(500).json(err);
-            })
-    })
+    DepositAmount(username, depositAmount)
+        .then((data) => {
+            res.status(202).json("Deposit Successfully");
+        })
+        .catch((err) => {
+            res.status(500).json(err);
+        })
 })
 
 
 router.post("/withdraw", (req, res) => {
     const { withdrawAmount } = req.body;
     const username = res.locals.username;
-    const sqlQueryUpdate = `update usersDetails set acc_bal=acc_bal - ${withdrawAmount} where username="${username}";`
-
-    // balance check
-    isInSufficientBalance(username, withdrawAmount)
+    WithdrawAmount(username, withdrawAmount)
         .then((data) => {
-            db.query(sqlQueryUpdate, (err, response) => {
-                if (err) {
-                    console.log(err);
-                    return res.status(500).json(err);
-                }
-                let payload = {
-                    amount: withdrawAmount,
-                    type: "withdraw",
-                    username: username
-                }
-                AddTransaction(payload)
-                    .then((transactionAdded) => {
-                        res.status(203).json(response);
-                    })
-                    .catch((err) => {
-                        console.log(err);
-                        res.status(500).json(err);
-                    })
-            })
+            res.status(200).json(data);
         })
         .catch((err) => {
-            return res.status(400).json("insufficient balance");
+            res.status(500).json(err);
         })
 
+})
+
+router.post("/transfer", (req, res) => {
+    const { transferAmount, userToTransfer } = req.body;
+    const username = res.locals.username;
+    TransferAmount(username, userToTransfer, transferAmount)
+        .then((withData, depositData) => {
+            res.status(202).json({ withData, depositData });
+        })
+        .catch((err) => {
+            res.status(500).json(err);
+        })
 })
 
 router.get("/current-balance", (req, res) => {
